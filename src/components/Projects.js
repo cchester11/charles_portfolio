32a92c8b8f0db3934a92c6f45d3b28ef8cdf26ba
Charles <cchester1@angelo.edu>
2021-07-06 17:07:39 -0500

    css and useState

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import photo1 from '../assets/projects-pictures/ss.png';
 import screenshot from '../assets/projects-pictures/screenshot.jpeg';
 import readmeGenerator from '../assets/projects-pictures/readmeGenerator.jpg';
 
-function Projects () {
+function Projects() {
+  const projectData = [
+    { name: "Fantasy Team Builder", link: "", image: photo1 },
+    { name: "Weather Dashboard", link: "", image: screenshot },
+    { name: "Readme Generator", link: "", image: readmeGenerator }
+  ]
+  const [toggle, setToggle] = useState(true)
   return (
     <div id="projects" className="mt-5 mb-5 d-flex flex-column justify-content-center align-content-center">
-      <h3 className="mt-5">Fantasy Team Builder</h3>
-      <img id="project-picture" src={photo1} alt="Fantasy Team Builder" className="my-1" />
-      <h3 className="mt-5">Weather Dashboard</h3>
-      <img id="project-picture" src={screenshot} alt="Weather Dashboard Project" className="my-1" />
-      <h3 className="mt-5">Readme Generator</h3>
-      <img id="project-picture" src={readmeGenerator} alt="readmeGenerator" className="my-1" />
+      <div style={toggle ?{color: "blue"}: {color: "red"}}>{toggle ?"Hello": "Goodbye"}</div>
+      <button type="button" onClick={() => setToggle(!toggle)}>Toggle</button>
+      {projectData.map(project => {
+        return (
+          <>
+            <h3 className="mt-5">{project.name}</h3>
+            <a href={project.link}>
+            <img id="project-picture" src={project.image} alt={project.name} className="my-1" />
+            </a>
+          </>
+        )
+      })}
     </div>
   )
 };
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Contact = () => (
-  <div id="contact-form" className="container-fluid d-flex align-items-center flex-column mt-5 mb-5 p-5">
-    <div id="user-contact" className="form-group flex-row">
-      {/* <label htmlFor="usr">Name:</label> */}
-      <input type="text" className="form-control" id="usr" placeholder={"Username"}/>
+const Contact = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [comments, setComments] = useState('');
+  const clearForm = () => {
+    setUsername('')
+    setEmail('')
+    setComments('')
+  };
+  return (
+    <div id="contact-form" className="container-fluid d-flex align-items-center flex-column mt-5 mb-5 p-5">
+      <div id="user-contact" className="form-group flex-row">
+        {/* <label htmlFor="usr">Name:</label> */}
+        <input type="text" value={username} onChange={event => setUsername(event.target.value)} className="form-control" id="usr" placeholder={"Username"} />
+      </div>
+      <div id="email-contact" className="form-group flex-row">
+        {/* <label htmlFor="pwd">Email:</label> */}
+        <input type="email" value={email} onChange={event => setEmail(event.target.value)} className="form-control" id="pwd" placeholder={"Email"} />
+      </div>
+      <div id="comment-contact" className="form-group flex-row">
+        {/* <label htmlFor="comment">Comment:</label> */}
+        <textarea className="form-control" value={comments} onChange={event => setComments(event.target.value)} rows={5} id="comment" placeholder={"Comments"} />
+      </div>
+      <button onClick={clearForm} type="button">Submit</button>
     </div>
-    <div id="email-contact" className="form-group flex-row">
-      {/* <label htmlFor="pwd">Email:</label> */}
-      <input type="password" className="form-control" id="pwd" placeholder={"Email"} />
-    </div>
-    <div id="comment-contact" className="form-group flex-row">
-      {/* <label htmlFor="comment">Comment:</label> */}
-      <textarea className="form-control" rows={5} id="comment" defaultValue={""} placeholder={"Comments"}/>
-    </div>
-    <button type="submit">Submit</button>
-  </div>
-);
+  )
+};
 
 export default Contact;
