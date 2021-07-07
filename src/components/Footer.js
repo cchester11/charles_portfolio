3c1d936783ef4058fa63fd8a3f37a333592898d1
Charles <cchester1@angelo.edu>
2021-07-07 13:31:41 -0500

    ..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,14 @@ function Footer () {
   return (
     <div id="footer" className="d-flex flex-row justify-content-between aling-content-center">
       <section>
+        <a id="github" href="https://github.com/cchester11" rel="noreferrer noopener" target="_blank">
         github: github.com/cchester11
+        </a>
       </section>
       <section>
-        linkedIn: linkedin.com/in/cchester11
+        <a id="linkedin" href="https://www.linkedin.com/in/chrlchester/" rel="noreferrer noopener" target="_blank">
+        linkedIn: linkedin.com/in/chrlchester
+        </a>
       </section>
     </div>
   )
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 const Resume = () => (
-  <div>
-    <h1>Resume</h1>
-    <p>Resume details</p>
-  </div>
+  <section>
+    <div className='proficiencies'>
+				<h2>Languages</h2>
+				<p>JavaScript ES6+, HMTL5, CSS3, SQL, NoSQL, GraphQL, Python</p>
+				<h2>Frameworks</h2>
+				<p>MongoDB, Express, Node, React, Jest</p>
+				<h2>Other Tools</h2>
+				<p>MySQL, jQuery, Bootstrap, UIKit, Foundations, Git (Github), Handlebars, Webpack, RESTful and non-RESTful APIs</p>
+			</div>
+  </section>
 );
 
 export default Resume;
